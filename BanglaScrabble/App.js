d0e534783cc8a4303a7d693786bbeb48c2896875
Ahnaf93 <ahnafkhantulon@gmail.com>
2019-10-09 17:02:50 +0600

    Screen 1 & 3v1

--- BanglaScrabble/App.js
+++ BanglaScrabble/App.js
@@ -1,22 +1,122 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View,TouchableOpacity,StyleSheet,TextInput , Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 6,
+    justifyContent: "center",
+    // alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: "black",
+    borderWidth: 1
+  },
+  submitButton: {
+    backgroundColor: "yellow",
+    width:160,
+    padding: 20,
+    margin: 10,
+    alignItems: "center",
+    height: 20
+    
+  },
+  submitButtonText: {
+    color: "black",
+    fontWeight: 'bold'
+  }
+});
+
+
+
 class HomeScreen extends React.Component {
+
+
+  joinin_ = () => {
+    this.props.navigation.navigate('Join')
+  
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+        
+       <TouchableOpacity
+          style={styles.submitButton}
+          //onPress={() => this.login(this.state.email, this.state.password)}
+          
+        >
+          <Text style={styles.submitButtonText}> New Game </Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity
+          style={styles.submitButton}
+          //onPress={() => this.login(this.state.email, this.state.password)}
+          onPress={this.joinin_}
+        >
+          <Text style={styles.submitButtonText}> Join Game </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-  },
-});
+class JoinScreen extends React.Component {
 
-export default createAppContainer(AppNavigator);
+  _gameOn = () => {
+    //function to make simple alert
+    alert('Message received, Waiting for Game UI');
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="Enter the code here"
+          placeholderTextColor="black"
+          autoCapitalize="none"
+          // onChangeText={(username)=>this.setState({username})}
+          // value={this.state.username}
+        />
+        
+
+
+        <TouchableOpacity
+          style={styles.submitButton}
+          //onPress={() => this.login(this.state.email, this.state.password)}
+
+          onPress={this._gameOn}
+        >
+          <Text style={styles.submitButtonText}> Alert Button </Text>
+        </TouchableOpacity>
+      </View>
+
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Join: JoinScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
