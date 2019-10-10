584af93b80e725420f27ff897d08322340acf66a
Muttakin Islam <islammuttakin@live.com>
2019-10-10 14:18:39 +0600

    list of games

--- App.js
+++ App.js
@@ -1,34 +1,58 @@
 import React from 'react';
-import { View,TouchableOpacity,StyleSheet,TextInput , Text } from 'react-native';
+import { FlatList,View,TouchableOpacity,StyleSheet,TextInput , Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 
 const styles = StyleSheet.create({
+  list: {
+    width:360,
+    flex: 3,
+    paddingTop: 22,
+    flexDirection:'column',
+    alignItems:'stretch',
+    borderColor:'red'
+
+   },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    //borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: 'black',
+  
+  },
   container: {
-    flex: 6,
-    justifyContent: "center",
-    // alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    flex: 1,
+   // justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    paddingTop: 22,
+    
+    
   },
   input: {
     margin: 15,
     height: 40,
+    width:300,
     borderColor: "black",
     borderWidth: 1
   },
   submitButton: {
-    backgroundColor: "yellow",
+    backgroundColor: "aqua",
     width:160,
     padding: 20,
-    margin: 10,
+    //margin: 10,
     alignItems: "center",
-    height: 20
+    height: 20,
+    margin: 10
     
   },
   submitButtonText: {
     color: "black",
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    alignItems: "center"
   }
 });
 
@@ -44,7 +68,7 @@ class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container}>
         
        <TouchableOpacity
           style={styles.submitButton}
@@ -62,6 +86,26 @@ class HomeScreen extends React.Component {
         >
           <Text style={styles.submitButtonText}> Join Game </Text>
         </TouchableOpacity>
+        <View style={{marginTop:20}}>
+        <Text style={styles.submitButtonText}> On Going Games </Text>
+        </View>
+        <View style={styles.list}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
       </View>
     );
   }
@@ -95,9 +139,30 @@ class JoinScreen extends React.Component {
 
           onPress={this._gameOn}
         >
-          <Text style={styles.submitButtonText}> Alert Button </Text>
+          <Text style={styles.submitButtonText}> Join Private Game </Text>
         </TouchableOpacity>
+        <View style={{marginTop:20}}>
+        <Text style={styles.submitButtonText}> On Going Public Games </Text>
+        </View>
+        <View style={styles.list}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
       </View>
+      </View>
+
 
     );
   }
