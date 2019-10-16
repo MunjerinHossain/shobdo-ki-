0a8adae4dbbc606adcd89e2d29096f560e08ab24
Shraboni <praptishraboni1@gmail.com>
2019-10-16 16:44:26 +0600

    reaction add

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import JoinScreen from './JoinScreen'
 import NewGameScreen from './NewGameScreen'
 import GameboardScreen from './GameboardScreen'
 import StatScreen from "./Stats"
+import Reaction from './Animation/Reaction'
 
 
 import NewGameOptionsScreen from './NewGameOptionsScreen'
@@ -20,6 +21,7 @@ const RootStack = createStackNavigator(
     Gameboard:GameboardScreen,
     GameOptionsScreen: NewGameOptionsScreen,
     Stats: StatScreen,
+    Reaction: Reaction
   },
   {
     initialRouteName: 'Home',
--- HomeScreen.js
+++ HomeScreen.js
@@ -100,10 +100,24 @@ export default class HomeScreen extends React.Component {
     
     }
 
+    reactionoptions_ = () => {
+      this.props.navigation.navigate('Reaction')
+    
+    }
+
     render() {
       return (
         <View style={styles.container}>
           
+          <TouchableOpacity
+            style={styles.submitButton}
+            //onPress={() => this.login(this.state.email, this.state.password)}
+            onPress={this.reactionoptions_}
+
+          >
+            <Text style={styles.submitButtonText}> Reaction </Text>
+          </TouchableOpacity>
+
          <TouchableOpacity
             style={styles.submitButton}
             //onPress={() => this.login(this.state.email, this.state.password)}
