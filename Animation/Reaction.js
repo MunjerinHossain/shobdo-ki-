0673fca314eef1017c2c5bb6409af79b8a7e249d
Shraboni <praptishraboni1@gmail.com>
2019-10-17 17:12:25 +0600

    add animation with button

--- Animation/Reaction.js
+++ Animation/Reaction.js
@@ -1,8 +1,119 @@
-import React, { Component } from 'react';
-import { Animated, TouchableWithoutFeedback,View, FlatList } from 'react-native';
+// import React, { Component } from 'react';
+// import {StyleSheet, Text, View,  SafeAreaView,TouchableOpacity } from "react-native";
+// import * as Animatable from "react-native-animatable";
+// import LottieView from 'lottie-react-native';
+
+// export default class Reaction extends Component{
+    
+//     render(){
+//         return(
+//             <SafeAreaView style={styles.container}>
+//             <View style={{ alignItems: "center" }}>
+//             <TouchableOpacity style={styles.btn} onPress={this._startAnimation}>
+//             <Text style={styles.textBtn}>Start animation</Text>
+//           </TouchableOpacity>
+//           <View style={{alignItems:"center"}}>   
+//                  <Animatable.View style={styles.card} animation="slideInDown" iterationCount= "5" direction="alternate">
+          
+//           <LottieView
+//              source={require('../Assets/Reaction/laugh.json')}
+//              loop
+//              autoPlay
+//             />
+            
+//           </Animatable.View>
+//           </View>
+
+//         </View>
+        
+//             </SafeAreaView>
+            
+           
+//         );
+       
+//     }
+    
+// }
+// const styles = StyleSheet.create({  
+//     container: {    flex: 1,    
+//         justifyContent: 'center',   
+//         alignItems: 'center',    
+//         backgroundColor: '#F5FCFF',
+    
+//     },
+//     card:{
+//      width:200,
+//      height:200,
+     
+//     },
+//     whiteText:{
+// textAlign:'center',
+// color:'white',
+//     },
+//     btn:{
+//         backgroundColor:'red',
+        
+
+//   color: 'red',
+//   padding: 10,
+//   textAlign:'center',
+  
+//     }
+// })
+ import React, { Component } from 'react';
+ import { StyleSheet, View, Button } from 'react-native'; 
+import * as Animatable from "react-native-animatable";
 import LottieView from 'lottie-react-native';
-import PropTypes from 'prop-types';
-import styles from './styles';
+ //import AnimatedLoader from 'react-native-animated-loader';
+ export default class Reaction extends Component { 
+        
+            state = { slideInDown: false }; 
+          
+          
+     handlePress = () => {   
+               setTimeout(() => {   
+                      this.setState(
+
+                          {                           slideInDown: true
+      
+                               });    }, ); 
+                          };
+                          render() {    
+                             const { slideInDown } = this.state;
+                          return (    
+                                   <View style={styles.container}>     
+                                   <Button title="press" onPress={this.handlePress} />       
+
+       <Animatable.View   style={styles.card} animation={this.state.slideInDown ? "slideInDown" : null} iterationCount= "5" direction="alternate">
+         <LottieView
+                       source={require('../Assets/Reaction/laugh.json')}
+                     />
+                   </Animatable.View>  
+                                      </View>   
+                                       ); 
+                                     }}
+                             const styles = StyleSheet.create({  
+                                 container: {    flex: 1,    
+                                     justifyContent: 'center',   
+                                     alignItems: 'center',    
+                                     backgroundColor: '#F5FCFF',
+                                 },
+                                 lottie: {   
+                                      width: 100,    
+                                    height: 100, 
+                                 },
+                                 card:{
+                                          width:200,
+                                          height:200,
+                                         
+                                         }
+                                });
+
+
+// import { Animated, TouchableWithoutFeedback,View, FlatList } from 'react-native';
+// import LottieView from 'lottie-react-native';
+// import PropTypes from 'prop-types';
+// import styles from './styles';
 //import { Card } from 'react-native-paper';
 
 // export default class Reaction extends Component{
@@ -22,17 +133,17 @@ import styles from './styles';
 //     }
 // }
 
-export default class Reaction extends Component {
-    constructor(props) {
-        super(props);
+// export default class Reaction extends Component {
+//     constructor(props) {
+//         super(props);
 
-        this.animatedValue = new Animated.Value(1);
-        this.animatedMargin = new Animated.Value(0);
-    }
+//         this.animatedValue = new Animated.Value(1);
+//         this.animatedMargin = new Animated.Value(0);
+//     }
 
-    componentDidMount() {
-        this.animation.play();
-    }
+//     componentDidMount() {
+//         this.animation.play();
+//     }
 
     // getReactionJson = type => {
     //     switch (type) {
@@ -45,66 +156,66 @@ export default class Reaction extends Component {
     //     }
     // };
 
-    onPressIn = () => {
-        Animated.spring(this.animatedValue, {
-            toValue: 2
-        }).start();
-        Animated.spring(this.animatedMargin, {
-            toValue: 16
-        }).start();
-    };
+//     onPressIn = () => {
+//         Animated.spring(this.animatedValue, {
+//             toValue: 2
+//         }).start();
+//         Animated.spring(this.animatedMargin, {
+//             toValue: 16
+//         }).start();
+//     };
 
-    onPressOut = () => {
-        Animated.spring(this.animatedValue, {
-            toValue: 1
-        }).start();
-        Animated.spring(this.animatedMargin, {
-            toValue: 0
-        }).start();
-    };
+//     onPressOut = () => {
+//         Animated.spring(this.animatedValue, {
+//             toValue: 1
+//         }).start();
+//         Animated.spring(this.animatedMargin, {
+//             toValue: 0
+//         }).start();
+//     };
 
-    render() {
-        const { type } = this.props;
-        const animatedStyle = {
-            transform: [{ scale: this.animatedValue }],
-            paddingBottom: this.animatedMargin
-        };
-        return (
-            <View style={styles.card}>
-            <TouchableWithoutFeedback
-                onPressIn={this.onPressIn}
-                onPressOut={this.onPressOut}
-            >
-                <Animated.View style={[styles.reactView, animatedStyle]}>
-                    <LottieView
-                        ref={animation => {
-                            this.animation = animation;
-                        }}
-                        style={styles.reaction}
-                        source={require('../Assets/Reaction/laugh.json')}
-                    />
-                </Animated.View>
+//     render() {
+//         const { type } = this.props;
+//         const animatedStyle = {
+//             transform: [{ scale: this.animatedValue }],
+//             paddingBottom: this.animatedMargin
+//         };
+//         return (
+//             <View style={styles.card}>
+//             <TouchableWithoutFeedback
+//                 onPressIn={this.onPressIn}
+//                 onPressOut={this.onPressOut}
+//             >
+//                 <Animated.View style={[styles.reactView, animatedStyle]}>
+//                     <LottieView
+//                         ref={animation => {
+//                             this.animation = animation;
+//                         }}
+//                         style={styles.reaction}
+//                         source={require('../Assets/Reaction/laugh.json')}
+//                     />
+//                 </Animated.View>
 
 
                 
-            </TouchableWithoutFeedback>
+//             </TouchableWithoutFeedback>
             
-                <FlatList
-                    data={this.types}
-                    horizontal
-                    renderItem={this.renderItem}
-                    keyExtractor={item => item.key}
-                    bounces={false}
-                    style={styles.list}
-                />
-            </View>
+//                 <FlatList
+//                     data={this.types}
+//                     horizontal
+//                     renderItem={this.renderItem}
+//                     keyExtractor={item => item.key}
+//                     bounces={false}
+//                     style={styles.list}
+//                 />
+//             </View>
 
 
-        );
-    }
-}
+//         );
+//     }
+// }
 
-Reaction.propTypes = {
-    type: PropTypes.string,
-    index: PropTypes.number
-};
+// Reaction.propTypes = {
+//     type: PropTypes.string,
+//     index: PropTypes.number
+// };
