8905ab982e346fa8da2b16518a233f4f7586bc68
RaiyanKhalil <raiyankhalil@gmail.com>
2019-10-28 11:26:23 +0600

    keyboard generated, frequency issue still exists

--- LetterLogic.js
+++ LetterLogic.js
@@ -11,93 +11,99 @@ var SampleArray = ["bhalo",
 "alo" ,
 "murgi", 
 "goru" ,
-"chagol"
+"chagol",
+"jambura",
+"zumka"
 ];
 export default class Letterlogic extends React.Component{
 
-    // converter1 =()=> {
-    //     //var SampleArray_2 = SampleArray.toString();
-    //     console.log(SampleArray)
-        
-    // }
-
-    // state = {
-    //     wordString: "hello"
-    // }
-  
-    // converter =()=> {
-    //     this.setState({
-    //         wordString: SampleArray.toString()
-    //     })
-    // }
-    //Keyboard 
-    removeComma =()=> {
+    //Keyboard Rules 
+    letterFrequency=()=> {
         var SampleArray_2 = SampleArray.toString();
         var Alphabets = "abcdefghijklmnopqrstuvwxyz"
         var CharFreq = {};
+
         for(var i = 0; i < Alphabets.length; i++){
             var count = 0;
             for(var j = 0; j < SampleArray_2.length; j++) {
                 if(SampleArray_2[j] == Alphabets[i]){
-                    //console.log(SampleArray_2[i])
                     count++;
                     
                 }
-                //console.log("it works") 
             }
-            
-            CharFreq[Alphabets[i]] = count
-           // console.log(Alphabets[i], count)
-           // CharFreq.push({okkhor: Alphabets[i], freq: count})
-           
-           
-            
+            CharFreq[Alphabets[i]] = count        
         }
-        console.log(CharFreq)
+        //console.log(CharFreq)
         //console.table(CharFreq)
         return CharFreq;
     }
 
-    logic = ()=> {
-        let letterscount = this.removeComma();
-        //console.log("asdasa")
-        //console.table(letterscount);
-
-        //const entries = Object.entries(letterscount) 
-        //console.log(entries)
-        var letters = [];
+    sortLetters=()=> {
+        let letterscount = this.letterFrequency();
+        //var letters = [];
+        var Letters = {};
+        
         for(let [key, value] of Object.entries(letterscount)) {
-
             if(value != 0){
-                letters.push([key, value])
-                
+                //Letters.push([key, value])
+                Letters[key] = value;
             }
         }
-        console.log(letters);
+        //console.log(Letters);
+        return Letters;
     }
-    render() {
+
+    generateKeyboard=()=> {
+        let keycount = this.sortLetters();
+        var letters1 = [];
+       // console.log(keycount)
+
+        //for(var i = 0; i < keycount.length; i++){
+           
+            // if(keycount[i] >= 5){
+            //     console.log("np")
+            // //    for(var j = 0; j < 3; j++){
+            // //         letters1.push(keycount[i])
+            // //     }
+            // }
+        //}
+        for(let [key, value] of Object.entries(keycount)) {
+            if( value >= 5){
+                for(var i = 0; i < 3; i++){
+                    letters1.push({key, value});
+                    
+                }
+                
+            }
+            else if(value < 5 && value > 2){
+                for(var i = 0; i <2; i++){
+                    letters1.push({key, value});
+                }
+            }
+            else{
+                for(var i = 0; i <1; i++){
+                    letters1.push({key, value});
+                }
+            }
+            
+        }
         
-        //console.log("asaa")
-        // var SampleArray = ["bhalo",
-        // "apu" ,
-        // "ma" ,
-        // "khela", 
-        // "bhai" ,
-        // "biral" ,
-        // "pani"  ,
-        // "morich",
-        // "alo" ,
-        // "murgi", 
-        // "goru" ,
-        // "chagol"
-        // ];
+        console.table(letters1);
+        //console.log(letters1)
+        // for(let [key, value] of Object.entries(keycount)) {
+        //     if(value == 5){
+        //         for(var i = 0; i < 3; i++){
+        //             letters1.push(key)
+        //         }
+        //     }
+        // } [["a",9]]
 
-        // var SampleArray_2 = SampleArray.toString();
+    }
 
-
+    render() {
         return (
             <View>
-                <Button title="Hello" onPress={()=>{this.logic()}}></Button>
+                <Button title="Hello" onPress={()=>{this.generateKeyboard()}}></Button>
                 {/* <Text>{this.state.wordString}</Text>  */}
                 {/* <Button title="Hello" onPress={()=>{this.converter()}}></Button>
                 <Text>{this.state.wordString}</Text>  */}
