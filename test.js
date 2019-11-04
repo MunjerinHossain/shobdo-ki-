cf53f4059362a8c0ea7b337a12dd9fbcc982c713
RaiyanKhalil <raiyankhalil@gmail.com>
2019-11-04 15:48:16 +0600

    validation works

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ let fs = require("fs")
 var words = []
 let text = fs.readFileSync("bl.js").toString().split(" ")
 for(var i = 0; i < text.length; i++){
-    words.push({[text[i]]:"easy"})
+    words.push(text[i])
 }
 
 try {
-    fs.writeFileSync('BanglaWordLists.js', JSON.stringify(words), { mode: 0o755 });
+    fs.writeFileSync('BanglaWordLists.js', JSON.stringify(words));
   } catch(err) {
     // An error occurred
     console.error(err);
